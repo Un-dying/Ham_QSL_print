b2853fe2e4128d98069c798ac71c36e6993ade0c
Z. B <92738360+Un-dying@users.noreply.github.com>
2023-05-07 22:11:20 +0800

    Create memo_cut.py

---
+++ memo_cut.py
@@ -0,0 +1,9 @@
+'''
+1.定义变量cut_1,cut_2,cut_end
+2.cut_end的值为'--------------------------------'
+'''
+
+# here put the import lib
+# 定义变量cut_1,cut_2,cut_end
+cut_end = '--------------------------------'
+cut_send = str(cut_end)
